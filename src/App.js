94af2db818b0aa15d474e2b5f13107cf538268f4
dianag76 <dianag766@gmail.com>
2022-08-04 10:32:00 -0700

    fixing errors

--- src/App.js
+++ src/App.js
@@ -1,69 +1,66 @@
-import React, {Component} from "react";
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { BrowserRouter, Route } from "react-router-dom";
 import Search from "./components/Search";
 import Nav from "./components/Nav";
-import PhotoList from './components/PhotoList';
+import PhotoList from "./components/PhotoList";
 // import NotFound from './components/NotFound';
 import apiKey from "./components/config";
-import axios from "axios";
 
 // const url =
 // `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`
 
 export default class App extends Component {
-  constructor(){
-  super();
-  this.state={
-    photos:[],
-  }
-
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading:true,
+    };
   }
   componentDidMount() {
-    this.searchPhoto()
+    this.performSearch(); 
+    // this.performSearch();
   }
 
-  searchPhoto=(query= "cheese") =>{
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.
-search&api_key=${apiKey}&tags=cats&per_page=24&format=json&
-nojsoncallback=1`)
-    .then(response => {
-      console.log(response.data.photos.photo);
-      this.setState({
-        photos: response.data.photos.photo
+  performSearch = (query = "cheese") => {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.
+search&api_key=${apiKey}&tags={query}&per_page=24&format=json&
+nojsoncallback=1`
+      )
+      .then((response) => {
+        console.log(response.data.photos.photo);
+        this.setState({
+          photos: response.data.photos.photo,
+          loading:false,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
       });
-    })
-    .catch(error => {
-      console.log('Error fetching and parsing data', error);
-    });    
-}
+  };
 
-  render(){
+  render() {
     console.log(this.state.photos);
-  return (
-    <div className = "App">
-
-    {/* // lassName="App"> */}
-    {/* // <Nav/> */}  
-    {/* // Found */}
-    {/* //  */}
-    <div className="container">  
-      <Search/>
-      <Nav/>
-
-        {/* <ul>
-          <li><a href="#">Cats</a></li>
-          <li><a href="#">Dogs</a></li>
-          <li><a href="#">Computers</a></li>
-        </ul> */}
-  
-      <div className="photo-container">
-        <h2>Results</h2>
-        <PhotoList data={this.state.photos}/>
-        {/* <NotFound/> */}
-      </div>
-    </div>
-    </div>
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="container">
+            <Search onSearch={this.performSearch} />
+            <Nav />
+            {this.state.loading ? (
+              <p>Loading...</p>
+            ) : (
+              <PhotoList data={this.state.photos} />
+            )}
+            <Route exact path="/" render={() => <PhotoList />} />
+            <Route exact path="/search" render={() => <Search />} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
+  }
 }
-}
-
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    <nav className="main-nav">
-        <ul>
-            {/* <li><NavLink href="#">Cats</NavLink></li> */}
-            {/* <li><NavLink href="#">Dogs</NavLink></li> */}
-            {/* <li><NavLink href="#">Computers</NavLink></li> */}
-            insideNav 
-        </ul>
-    </nav>
-}
+  <nav className="main-nav">
+    <ul>
+      <li>
+        <NavLink href="/cats">Cats</NavLink>
+      </li>
+      <li>
+        <NavLink href="/dogs">Dogs</NavLink>
+      </li>
+      <li>
+        <NavLink href="/computers">Computers</NavLink>
+      </li>
+    </ul>
+  </nav>;
+};
 
 export default Nav;

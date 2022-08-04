1610f17d987bcdf7933f8ddd104a126451cc0659
dianag76 <dianag766@gmail.com>
2022-08-04 08:50:30 -0700

    Trying on map but getting an error. Dummy data was rendering before that

--- src/App.js
+++ src/App.js
@@ -3,21 +3,49 @@ import './App.css';
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 import PhotoContainer from './components/PhotoContainer';
-import NotFound from './components/NotFound';
+// import NotFound from './components/NotFound';
+import apiKey from "./components/config";
+import axios from "axios";
 
-const url =`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`
+// const url =
+// `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1`
 
 export default class App extends Component {
   constructor(){
   super();
+  this.state={
+    photos:[],
   }
 
+  }
+  componentDidMount() {
+    this.searchPhoto()
+  }
+
+  searchPhoto=(query= "cheese") =>{
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.
+search&api_key=${apiKey}&tags=cats&per_page=24&format=json&
+nojsoncallback=1`)
+    .then(response => {
+      console.log(response.data.photos.photo);
+      this.setState({
+        photos: response.data.photos.photo
+      });
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });    
+}
+
   render(){
+    console.log(this.state.photos);
   return (
-    // lassName="App">
-    // <Nav/> */} 
-    // Found
-    // 
+    <div className = "App">
+
+    {/* // lassName="App"> */}
+    {/* // <Nav/> */}  */}
+    {/* // Found */}
+    {/* //  */}
     <div className="container">  
       <Search/>
       <Nav/>
@@ -31,8 +59,9 @@ export default class App extends Component {
       <div className="photo-container">
         <h2>Results</h2>
         <PhotoContainer/>
-        <NotFound/>
+        {/* <NotFound/> */}
       </div>
+    </div>
     </div>
     );
 }
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,22 +1,26 @@
 import React from "react";
+import Photo from "./Photo"; 
 
-const PhotoContainer = ()=>{
+const PhotoContainer = (props)=>{
+    const results = props.data
+    let photos;
+     photos = results.map(photo=> (
+        <Photo 
+        key={photo.id} 
+        url={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg`}
+        />
+     )
+
+        )
     return(
-        <ul>
-            <li>
-                <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-            </li>
-            <li>
-                <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-            </li>
-            <li>
-                <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-            </li>
-            <li>
-                <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-            </li>
-
-        </ul>
+        <div className="photo-container">
+           <h2>
+            Results
+           </h2>
+           <ul>
+            {photos}
+           </ul>
+         </div>
     )  
 }
 
